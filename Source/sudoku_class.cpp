e2bde3d2f0e9de9de0522bf779c9d34c08b04630
Max <smi97@mail.ru>
2018-01-10 14:12:49 +0300

    New methods
    Added 3 new methods and made code easier. Works better at all the tests.

--- Source/sudoku_class.cpp
+++ Source/sudoku_class.cpp
@@ -4,12 +4,30 @@ Sudoku::Sudoku(std::deque<std::deque<int>> input)
 {
     rec_flag = false;
     sudoku.resize(9);
-    for (int i = 0, size = sudoku.size(); i < size; ++i)   sudoku[i] = input[i];
+    for (int i = 0, size = sudoku.size(); i < size; ++i)
+        sudoku[i] = input[i];
+
 }
 
 std::deque<std::deque<int>> Sudoku::get()
 {
     return sudoku;
+}
+
+void Sudoku::print(std::deque<std::deque<int>> sud)
+{
+    for(int row = 0; row < sud.size(); row++)
+    {
+        for(int col = 0; col < sud.size(); col++)
+        {
+            if(sud[row][col] != -1) std::cout << sud[row][col];
+            else std::cout << ".";
+            std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
 }
 
 std::set<int> Sudoku::check_row(std::deque<std::deque<int>> sud, int row)
@@ -51,24 +69,21 @@ std::set<int> Sudoku::check_square(std::deque<std::deque<int>> sud, int row, int
 }
 
 
-std::set<int> Sudoku::check_xy(std::deque<std::deque<int>> sud, int row, int col)
+std::unordered_set<int> Sudoku::check_xy(std::deque<std::deque<int>> sud, int row, int col)
 {
-    std::set<int> rows, cols, squs, checked;
+    std::set<int> rows, cols, squs;
+    std::unordered_set<int> checked;
     rows = check_row(sud, row);
     cols = check_column(sud, col);
     squs = check_square(sud, row, col);
-    rows.insert(cols.begin(), cols.end());
-    rows.insert(squs.begin(), squs.end());
 
-    bool fl[9];
     for(int i = 0; i < 9; i++)
-        fl[i] = true;
-    for(auto it : rows)
     {
-        fl[it - 1] = false;
+        if(rows.find(i + 1) == rows.end() &&
+           cols.find(i + 1) == cols.end() &&
+           squs.find(i + 1) == squs.end())
+            checked.insert(i + 1);
     }
-    for(int i = 0; i < 9; i++)
-        if(fl[i]) checked.insert(i + 1);
     return checked;
 }
 
@@ -76,37 +91,38 @@ void Sudoku::check_sud(std::deque<std::deque<int>> sud)
 {
     if(rec_flag)
         return;
-    int pos_row = 0;
+
     bool flag = true;
-    for(auto it = sud.begin(); it != sud.end(); it++)
+
+
+    for(int row = 0; row < sud.size(); row++)
     {
-        int pos_col = 0;
-        for(auto it1 = it->begin(); it1 != it->end(); it1++)
+        for(int col = 0; col < sud.size(); col++)
         {
-            if(*it1 == -1)
+            if(sud[row][col] == -1)
             {
                 flag = false;
-                auto temp = check_xy(sud, pos_row, pos_col);
+                auto temp = check_xy(sud, row, col);
                 if(temp.size() == 1)
                 {
-                    *it1 = *temp.begin();
+                    sud[row][col] = *temp.begin();
                 }
                 if(temp.size() == 0)
+                {
                     return;
+                }
                 if(temp.size() > 1)
                 {
-                    for(auto it2 : temp)
+                    for(auto it : temp)
                     {
-                        *it1 = it2;
+                        sud[row][col] = it;
                         check_sud(sud);
                         if(rec_flag)
                             return;
                     }
                 }
             }
-            pos_col++;
         }
-        pos_row++;
     }
     if(flag)
     {
@@ -119,8 +135,154 @@ void Sudoku::check_sud(std::deque<std::deque<int>> sud)
     return;
 }
 
+void Sudoku::stand_method()
+{
+
+    bool flag = true;
+    for(int row = 0; row < sudoku.size(); row++)
+    {
+        for(int col = 0; col < sudoku.size(); col++)
+        {
+            
+            if(sudoku[row][col] == -1)
+            {
+                flag = false;
+                auto temp = check_xy(sudoku, row, col);
+                if(temp.size() == 1)
+                {
+                    sudoku[row][col] = *temp.begin();
+                }
+            }
+        }
+    }
+}
+
+
 void Sudoku::solve()
 {
+    std::deque<std::deque<int>> sud = sudoku;
+
+    stand_method();
+    while(sud != sudoku)
+    {
+        while(sud != sudoku)
+        {
+            sud = sudoku;
+            stand_method();
+        }
+        sud = sudoku;
+        sudoku = check_row_ones(sudoku);
+        while(sud != sudoku)
+        {
+            sud = sudoku;
+            stand_method();
+        }
+        sudoku = check_col_ones(sudoku);
+    }
+
     check_sud(sudoku);
+
+
+}
+
+std::deque<std::deque<int>> Sudoku::check_row_ones(std::deque<std::deque<int>> sud)
+{
+    int mem[9];
+    for(int i = 0; i < 9; i++)
+    {
+        mem[i] = 0;
+    }
+    for(int row = 0; row < sudoku.size(); row++)
+    {
+        for(int col = 0; col < sudoku.size(); col++)
+        {
+            if(sudoku[row][col] == -1)
+            {
+                auto temp = check_xy(sudoku, row, col);
+                if(temp.size() > 1)
+                {
+                    for(auto it : temp)
+                    {
+                        mem[it - 1]++;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < 9; i++)
+        {
+            if(mem[i] == 1)
+            {
+                for(int col = 0; col < sudoku.size(); col++)
+                {
+                    if(sudoku[row][col] == -1)
+                    {
+                        auto temp = check_xy(sudoku, row, col);
+                        if(temp.size() > 1)
+                        {
+                            for(auto it : temp)
+                            {
+                                if(it - 1 == i)
+                                    sud[row][col] = i + 1;
+                            }
+                        }
+                    }
+                }
+            }
+            mem[i] = 0;
+        }
+    }
+    return sud;
+}
+
+std::deque<std::deque<int>> Sudoku::check_col_ones(std::deque<std::deque<int>> sud)
+{
+    int mem[9];
+    for(int i = 0; i < 9; i++)
+    {
+        mem[i] = 0;
+    }
+    for(int col = 0; col < sudoku.size(); col++)
+    {
+        for(int row = 0; row < sudoku.size(); row++)
+        {
+            if(sudoku[row][col] == -1)
+            {
+                auto temp = check_xy(sudoku, row, col);
+                if(temp.size() > 1)
+                {
+                    for(auto it : temp)
+                    {
+                        mem[it - 1]++;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < 9; i++)
+        {
+            if(mem[i] == 1)
+            {
+                for(int row = 0; row < sudoku.size(); row++)
+                {
+                    if(sudoku[row][col] == -1)
+                    {
+                        auto temp = check_xy(sudoku, row, col);
+                        if(temp.size() > 1)
+                        {
+                            for(auto it : temp)
+                            {
+                                if(it - 1 == i)
+                                    sud[row][col] = i + 1;
+                            }
+                        }
+                    }
+                }
+            }
+            mem[i] = 0;
+        }
+    }
+
+    return sud;
 }
 
--- Source/sudoku_class.hpp
+++ Source/sudoku_class.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <algorithm>
 #include <set>
+#include <unordered_set>
 
 
 class Sudoku
@@ -15,7 +16,12 @@ private:
     std::set<int> check_row(std::deque<std::deque<int>> sud, int row);
     std::set<int> check_column(std::deque<std::deque<int>> sud, int col);
     std::set<int> check_square(std::deque<std::deque<int>> sud, int row, int col);
-    std::set<int> check_xy(std::deque<std::deque<int>> sud, int row, int col);
+    std::unordered_set<int> check_xy(std::deque<std::deque<int>> sud, int row, int col);
+    std::deque<std::deque<int>> check_row_ones(std::deque<std::deque<int>> sud);
+    std::deque<std::deque<int>> check_col_ones(std::deque<std::deque<int>> sud);
+    void print(std::deque<std::deque<int>> sud);
+
+    void stand_method();
 
     void check_sud(std::deque<std::deque<int>> sud);
 
